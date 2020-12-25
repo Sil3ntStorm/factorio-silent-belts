cd6c61e0e8da0b67aa25f238f603e953a7c6545d
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2020-12-25 02:05:33 +0100

    Factorio 0.16 by Wakaba-chan (https://mods.factorio.com/mod/SilentBelts3)

---
+++ data-final-fixes.lua
@@ -0,0 +1,3 @@
+for _, entity in pairs(data.raw["transport-belt"]) do	
+	entity.working_sound.sound.volume = 0.0
+end
--- data.lua
+++
@@ -1,3 +0,0 @@
-data.raw["transport-belt"]["transport-belt"].working_sound.sound.volume = 0.0
-data.raw["transport-belt"]["fast-transport-belt"].working_sound.sound.volume = 0.0
-data.raw["transport-belt"]["express-transport-belt"].working_sound.sound.volume = 0.0
